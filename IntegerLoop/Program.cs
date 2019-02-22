119b990792ed2e93d13a5295e2e7afcd5462c526
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-02-22 16:35:02 -0600

    Finished Homework

--- IntegerLoop/Program.cs
+++ IntegerLoop/Program.cs
@@ -6,6 +6,13 @@ namespace IntegerLoop
     {
         public static void Main()
         {
+            for (int i = 0; i < 51; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
             // TODO: Print even integers from 0 to 50.
 
             Console.ReadLine();
