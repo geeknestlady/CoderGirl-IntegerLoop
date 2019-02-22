d63ea691f9d9619f10406fb1305162ef7641064f
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-02-22 17:08:58 -0600

    Finished Homework with correction, program not expecting 0 to be in the list.

--- IntegerLoop/Program.cs
+++ IntegerLoop/Program.cs
@@ -5,15 +5,17 @@ namespace IntegerLoop
     public static class Program
     {
         public static void Main()
-        {
-            for (int i = 0; i < 51; i++)
+        {           
+            for (int i = 1; i <= 50; i++)
             {
                 if(i % 2 == 0)
-                {
-                    Console.WriteLine(i);
+                {                   
+                    Console.WriteLine(i);                                       
                 }
+                
             }
-            // TODO: Print even integers from 0 to 50.
+           
+            // TODO: Print even integers from 0 to 50
 
             Console.ReadLine();
         }
